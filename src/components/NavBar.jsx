c496077009413b00a04b7f5e3b2e552f3c7d68ed
Dev_Faruq <olatoyesefaruq@gmail.com>
2025-02-02 02:05:35 +0100

    fix: add hover effect to my button

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -23,12 +23,14 @@ function NavBar() {
       }`}
     >
       <div className="max-w-[1200px] mx-auto flex justify-between items-center px-4">
-        <div><img src="/images/DesignForge.png" alt="logo" /></div>
+        <div>
+          <img src="/images/DesignForge.png" alt="logo" />
+        </div>
         <div className="flex gap-4 text-base font-bold">
-          <button className="flex justify-center px-6 py-3 items-center border border-[#1A1B1C] text-[#1A1B1C] rounded-lg">
+          <button className="flex justify-center px-6 py-3 items-center border border-[#1A1B1C] text-[#1A1B1C] rounded-lg hover:bg-[#f0f0f0] cursor-pointer transition-all duration-300">
             Login
           </button>
-          <button className="flex justify-center px-6 py-3 items-center bg-[#1A1B1C] text-white rounded-lg">
+          <button className="flex justify-center px-6 py-3 items-center bg-[#1A1B1C] text-white rounded-lg hover:bg-[#333333] cursor-pointer transition-all duration-300">
             Create Account
           </button>
         </div>
