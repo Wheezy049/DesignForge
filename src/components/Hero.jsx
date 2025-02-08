7c46e3c988340ef49322fc6399f19ed625d40e84
Dev_Faruq <olatoyesefaruq@gmail.com>
2025-02-08 19:17:41 +0100

    fix: change my hero section text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ function Hero() {
   return (
     <div className="flex justify-center gap-2 md:gap-4 flex-col items-center font-darkerGrotesque w-full max-w-[1200px] text-[#25292E]">
       <div className="text-center text-[40px] md:text-[64px] leading-[45px] md:leading-[68px] font-bold w-[300px] md:w-[556px]">
-        <h1>Open-Source <span className=" font-extrabold">Designer</span>-Focused</h1>
+        <h1>Design the Next Big <span className=" font-extrabold">Open-Source</span> Project</h1>
       </div>
       <div className="text-center text-[20px] md:text-[32px] leading-[24px] md:leading-[36px] font-medium w-[282px] md:w-[556px]">
         <p>
